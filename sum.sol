f700b288404cb220a9a1bae8de52a9b662238287
kaizokubhaiya <abhishekiskaizoku@gmail.com>
2023-06-12 16:52:28 +0530

    sum

--- sum.sol
+++ sum.sol
@@ -1,11 +1,28 @@
-pragma solidity ^0.5.0;
-contract SolidityTest {
-   constructor() public{
-   }
-   function getResult() public view returns(uint){
-      uint a = 1;
-      uint b = 2;
-      uint result = a + b;
-      return result;
-   }
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.4.16 < 0.9.0;
+
+
+contract SimpleSum
+{
+	
+	uint public var1;
+	uint public var2;
+	uint public sum;
+
+	
+	function set(uint x, uint y) public
+	{
+		var1 = x;
+		var2 = y;
+		sum=var1+var2;
+	}
+	
+	
+	function get(
+	) public view returns (uint) {
+		return sum;
+	}
 }
+
+//use remix ide and set values in deploy and run txn
+//use get or sum func to get the sum
