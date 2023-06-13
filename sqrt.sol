c6e286d0e5317c513b559a85d3b96133c2736391
kaizokubhaiya <abhishekiskaizoku@gmail.com>
2023-06-13 14:54:25 +0530

    v1.1

--- sqrt.sol
+++ sqrt.sol
@@ -2,17 +2,18 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Test {
-	//Defining public pure sqrt function
 	function sqrt(uint _num) public pure returns(uint){
 	_num = _num ** 2;
 	return _num;
 	}
-	// Defining a public pure function to demonstrate
-	// calling of sqrt function
+
 	function add() public pure returns(uint){
 	uint num1 = 100;
 	uint num2 = 16;
 	uint sum = num1 + num2;
-	return sqrt(sum); // calling function
+	return sqrt(sum); // calling
+
 }
-}
+
+//use remix ide and set values in deploy and run txn
+//use add and sqrt function
